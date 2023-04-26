58ccab35d5132f4fc2943766f03313ac04e0742d
raquel-castillejo <raquelcastillejo@pm.me>
2023-04-26 23:08:15 +0200

    práctica de angular con operaciones matemáticas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'operators';
+  number1 !: number;
+  number2 !: number;
+
+  addition = false;
+  substract = false;
+  product = false;
+  division = false;
+
+  result !: string;
+
+  operate() {
+    this.result = "";
+
+    if(this.addition) {
+      let solved = this.number1 + this.number2;
+      this.result += `La suma de los números ${this.number1} y ${this.number2} es ${solved}.`;
+    };
+
+    if(this.substract) {
+      let solved = this.number1 - this.number2;
+      this.result += `La resta de los números ${this.number1} y ${this.number2} es ${solved}.`;
+    };
+
+    if(this.product) {
+      let solved = this.number1 * this.number2;
+      this.result += `La multiplicación de los números ${this.number1} y ${this.number2} es ${solved}.`;
+    };
+
+    if(this.division) {
+      let solved = this.number1 / this.number2;
+      this.result += `La división de los números ${this.number1} y ${this.number2} es ${solved}.`;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// importamos elementos necesarios para el ejercicio: formularios, input, botón y checkbox
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
+// añadidos míos:
+// divisor
+import {MatDividerModule} from '@angular/material/divider';
+// tarjeta
+import {MatCardModule} from '@angular/material/card'; 
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+
+    // importamos elementos necesarios para el ejercicio: formularios, input, botón y checkbox
+		FormsModule,
+		MatButtonModule,
+		MatInputModule,
+		MatCheckboxModule,
+    MatDividerModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
